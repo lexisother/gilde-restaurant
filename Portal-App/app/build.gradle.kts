c047a943d1641ebabbd3e44f2bfd50c414505c84
Alyxia <alyxia@riseup.net>
2022-09-10 15:20:50 +0200

    [Portal-App] Bump deps

--- Portal-App/app/build.gradle.kts
+++ Portal-App/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("kotlin-android")
 }
 
-val compose_version = "1.0.1"
+val compose_version = "1.2.0"
 
 android {
     compileSdk = 33
@@ -48,17 +48,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.compose.ui:ui:$compose_version")
     implementation("androidx.compose.material:material:$compose_version")
     implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
     implementation("androidx.compose.material:material-icons-extended:$compose_version")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha08")
+    implementation("androidx.navigation:navigation-compose:2.6.0-alpha01")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.18.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.5.1")
     implementation("com.squareup.okhttp3:okhttp:3.10.0")
     implementation("com.beust:klaxon:5.5")
     debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
