e4dddf5f3a075646ca7f832880a91271ff8ed36c
Alyxia <alyxia@riseup.net>
2022-09-12 21:57:08 +0200

    [API] Set up health route

--- API/index.php
+++ API/index.php
@@ -1,4 +1,21 @@
 <?php
-header("Content-Type: application/json");
+require __DIR__ . "/vendor/autoload.php";
 
-echo '{"test": {"hi": "yes"}}';
+use Bramus\Router\Router;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+$whoops = new Run();
+$whoops->pushHandler(new PrettyPageHandler());
+$whoops->register();
+
+header("Content-Type: application/json");
+$router = new Router();
+
+$router->mount("/api", function () use ($router) {
+    $router->all("/health", function () {
+        echo '{"ok": true}';
+    });
+});
+
+$router->run();
