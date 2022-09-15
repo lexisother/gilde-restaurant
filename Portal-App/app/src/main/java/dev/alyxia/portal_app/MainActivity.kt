af0a393a85f4665136448776c916ef24feae8bca
Alyxia <alyxia@riseup.net>
2022-09-15 20:48:31 +0200

    [Portal-App] Move to NavHost, snackbar error popup

--- Portal-App/app/src/main/java/dev/alyxia/portal_app/MainActivity.kt
+++ Portal-App/app/src/main/java/dev/alyxia/portal_app/MainActivity.kt
@@ -3,53 +3,15 @@ package dev.alyxia.portal_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import dev.alyxia.portal_app.rest.dto.ApiHealth
-import dev.alyxia.portal_app.rest.dto.ApiResponse
-import dev.alyxia.portal_app.rest.service.APIHealthServiceImpl
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.alyxia.portal_app.composables.Main
 import dev.alyxia.portal_app.ui.theme.PortalAppTheme
-import io.ktor.client.*
-import io.ktor.client.engine.android.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
-
-/**
- * @author X1nto
- */
-class EpicViewModel : ViewModel() {
-    private val client = HttpClient(Android) {
-        install(ContentNegotiation) {
-            json(Json {
-                ignoreUnknownKeys = true
-            })
-        }
-    }
-    var result by mutableStateOf<ApiResponse<ApiHealth>?>(null)
-        private set
-
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            result = APIHealthServiceImpl(client).checkHealth()
-        }
-    }
-}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,38 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val viewModel: EpicViewModel by viewModels()
-
-                    if (viewModel.result == null) {
-                        Column(
-                            modifier = Modifier
-                                .wrapContentHeight()
-                                .fillMaxWidth(),
-                            verticalArrangement = Arrangement.spacedBy(10.dp)
-                        ) {
-                            CircularProgressIndicator(
-                                modifier = Modifier
-                                    .wrapContentSize()
-                                    .align(Alignment.CenterHorizontally)
-                            )
-                            Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "Loading...")
-                        }
-                    } else {
-                        val res: Boolean?
-                        var err: String? = null
-                        when (val result = viewModel.result) {
-                            is ApiResponse.Success -> res = result.data.ok
-                            is ApiResponse.Error -> {
-                                err = result.error.error.type
-                                res = false
-                            }
-                            else -> res = false
-                        }
-
-                        Column {
-                            Text(text = "API is ok: $res")
-                            if (err !== null) Text(text = err)
-                        }
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "main") {
+                        composable("main") { Main(navController) }
                     }
                 }
             }
---
+++ Portal-App/app/src/main/java/dev/alyxia/portal_app/composables/Main.kt
@@ -0,0 +1,105 @@
+package dev.alyxia.portal_app.composables
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import dev.alyxia.portal_app.rest.dto.ApiHealth
+import dev.alyxia.portal_app.rest.dto.ApiResponse
+import dev.alyxia.portal_app.rest.service.APIHealthServiceImpl
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+
+/**
+ * @author X1nto
+ */
+class MainViewModel : ViewModel() {
+    private val client = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+    var result by mutableStateOf<ApiResponse<ApiHealth>?>(null)
+        private set
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            result = APIHealthServiceImpl(client).checkHealth()
+        }
+    }
+}
+
+@Composable
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+fun Main(
+    navController: NavController,
+    viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+) {
+    val scaffoldState = rememberScaffoldState()
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        if (viewModel.result == null) {
+            Column(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    text = "Loading..."
+                )
+            }
+        } else {
+            val res: Boolean?
+            var err: String? = null
+            when (val result = viewModel.result) {
+                is ApiResponse.Success -> res = result.data.ok
+                is ApiResponse.Error -> {
+                    err = result.error.error.type
+                    res = false
+                }
+                else -> res = false
+            }
+
+            Column {
+                Text(text = "API is ok: $res")
+                if (err !== null) {
+                    var isDialogVisible by remember { mutableStateOf(false) }
+                    LaunchedEffect(scaffoldState.snackbarHostState) {
+                        val res =
+                            scaffoldState.snackbarHostState.showSnackbar(
+                                message = "E",
+                                actionLabel = "E"
+                            )
+
+                        when (res) {
+                            SnackbarResult.ActionPerformed -> {
+                                // TODO: Show modal
+                            }
+                            SnackbarResult.Dismissed -> {}
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
