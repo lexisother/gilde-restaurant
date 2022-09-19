6519cc1af2cc390adb736b311bbeaea767f2a23b
Luciano <Luciano.waterberg@student.gildeopleidingen>
2022-09-19 10:00:48 +0200

    [Homepage] Basis pagina

---
+++ Homepage/Homepage.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Homepage.css">
+    <a href="images"></a>
+    <title>Holly Guacamoley</title>
+</head>
+
+
+<body>
+<?php 
+    include 'header.php'; 
+?> 
+
+
+<div class="front-text">
+   <center><div class="main-text">WELKOM!</div></center>
+   <center><div class="text1">HOLY GUACAMOLEY</div></center>
+   <center><div class="text2">De lekkerste tapas en drankjes haal je bij ons!</div></center>
+</div>
+
+<div class="container">
+<div class="black-header"></div>
+
+<h1>Holy Guacamoley</h1>
+<h2>Kom gezellig naar Holy Guacamoley!</h2>
+
+</body>
+
+
+<div class="menu-image">
+
+
+</div>
+
+
+
+</html>
+    
---
+++ Homepage/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="Header.css">
+    <a href="images"></a>
+    <title>Holly Guacamoley</title>
+</head>
+
+<body>
+
+<header>
+    <nav>
+        <ul class="nav_links">
+           <li><a href="Homepage.php">WELKOM</a></li>
+           <li><a href="Menu.php">MENU</a></li>
+           <li><a href="Openingstijden&Prijzen.php">RESERVERING</a></li>
+        </ul>
+    </nav>
+    <a class="cta" href="Contact.php"><button>CONTACT</button></a>
+</header>  
+
+
+
+    
+</body>
+</html>
+
+
+
+
