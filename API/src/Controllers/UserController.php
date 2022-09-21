53a2c6eccffeca256c600a76c8c0a3b8d101aeb1
Alyxia <alyxia@riseup.net>
2022-09-21 22:09:13 +0200

    [API] Start work on registering users

--- API/src/Controllers/UserController.php
+++ API/src/Controllers/UserController.php
@@ -1,10 +1,34 @@
 <?php
 
+namespace App\Controllers;
+
+use App\Models\User;
+use Exception;
+
 class UserController extends Controller
 {
+    /**
+     * @throws Exception
+     */
     public function registerUser()
     {
+        // Check if this is a form request or a POST request:
+        // TODO: Test if this actually works.
+        if (isset($_POST['email'])) {
+            $data = $_POST;
+        } else {
+            $data = json_decode(file_get_contents("php://input"));
+        }
 
+        if (!empty((array)$data)) {
+            if (!User::where('email', $data->email)->exists()) {
+                // TODO: Create a user here.
+            } else {
+                throw new Exception("User with this email already exists!");
+            }
+        } else {
+            throw new Exception("No request body was provided.");
+        }
     }
 
     public function loginUser()
