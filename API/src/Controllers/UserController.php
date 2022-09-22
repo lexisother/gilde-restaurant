8d81dc3611e93bdb2f6589db4b490d4820479c4a
Alyxia Sother <alyxia@riseup.net>
2022-09-22 12:45:33 +0200

    [API] Create a user during registration

--- API/src/Controllers/UserController.php
+++ API/src/Controllers/UserController.php
@@ -3,10 +3,25 @@
 namespace App\Controllers;
 
 use App\Models\User;
+use App\DTO\User as DTOUser;
 use Exception;
 
 class UserController extends Controller
 {
+    private function checkRegData($data) {
+        if (empty($data->username)) {
+            throw new Exception("Username is required.");
+        }
+
+        if (empty($data->email)) {
+            throw new Exception("Email is required.");
+        }
+
+        if (empty($data->password)) {
+            throw new Exception("Password is required.");
+        }
+    }
+
     /**
      * @throws Exception
      */
@@ -15,14 +30,24 @@ class UserController extends Controller
         // Check if this is a form request or a POST request:
         // TODO: Test if this actually works.
         if (isset($_POST['email'])) {
-            $data = $_POST;
+            $data = (object) $_POST;
         } else {
             $data = json_decode(file_get_contents("php://input"));
         }
 
+        // Check if the data is correct.
+        $this->checkRegData($data);
+
         if (!empty((array)$data)) {
             if (!User::where('email', $data->email)->exists()) {
-                // TODO: Create a user here.
+                $pass = md5($data->password);
+                $dtoUser = new DTOUser(0, $data->name, $data->email, $pass);
+                $user = new User([
+                   'name' => $dtoUser->name,
+                   'email' => $dtoUser->email,
+                   'password' => $dtoUser->password
+                ]);
+                $user->save();
             } else {
                 throw new Exception("User with this email already exists!");
             }
--- API/src/DTO/User.php
+++ API/src/DTO/User.php
@@ -14,18 +14,26 @@ class User
     public function __construct($id, $name, $email, $password)
     {
         if (is_int($id)) {
+            $this->id = $id;
+        } else {
             throw new InvalidArgumentException('$id must be an integer');
         }
 
         if (is_string($name)) {
+            $this->name = $name;
+        } else {
             throw new InvalidArgumentException('$name must be a string');
         }
 
         if (is_string($email)) {
+            $this->email = $email;
+        } else {
             throw new InvalidArgumentException('$email must be a string');
         }
 
         if (is_string($password)) {
+            $this->password = $password;
+        } else {
             throw new InvalidArgumentException('$password must be a string');
         }
     }
