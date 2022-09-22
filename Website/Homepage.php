bcc0e26578cf41affc69b119d189a98d9b5563a5
Milan <milanensink01@gmail.com>
2022-09-22 10:37:27 +0200

    File layout changes

--- Website/Homepage.php
+++ Website/Homepage.php
@@ -10,7 +10,7 @@
 
 <body>
 <?php 
-    include 'header.php'; 
+    include 'header/header.php'; 
 ?> 
 
 
---
+++ Website/menu/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <a href="images"></a>
+    <title>Holly Guacamoley MenuKaart</title>
+</head>
+
+
+<body>
+<?php 
+    include 'header/header.php'; 
+?> 
+
+</body>
+</html>
+    
