03e0ae0634e1856ed23d2b322c6bc3fec75a55e2
Alyxia <alyxia@riseup.net>
2022-09-26 20:28:53 +0200

    [API] Remove requirement of name for login

--- API/src/Controllers/UserController.php
+++ API/src/Controllers/UserController.php
@@ -2,26 +2,12 @@
 
 namespace App\Controllers;
 
-use App\Models\User;
 use App\DTO\User as DTOUser;
+use App\Models\User;
 use Exception;
 
 class UserController extends Controller
 {
-    private function checkRegData($data) {
-        if (empty($data->name)) {
-            throw new Exception("Name is required.");
-        }
-
-        if (empty($data->email)) {
-            throw new Exception("Email is required.");
-        }
-
-        if (empty($data->password)) {
-            throw new Exception("Password is required.");
-        }
-    }
-
     /**
      * @throws Exception
      */
@@ -30,7 +16,7 @@ class UserController extends Controller
         // Check if this is a form request or a POST request:
         // TODO: Test if this actually works.
         if (isset($_POST['email'])) {
-            $data = (object) $_POST;
+            $data = (object)$_POST;
         } else {
             $data = json_decode(file_get_contents("php://input"));
         }
@@ -43,9 +29,9 @@ class UserController extends Controller
                 $pass = md5($data->password);
                 $dtoUser = new DTOUser(0, $data->name, $data->email, $pass);
                 $user = new User([
-                   'name' => $dtoUser->name,
-                   'email' => $dtoUser->email,
-                   'password' => $dtoUser->password
+                    'name' => $dtoUser->name,
+                    'email' => $dtoUser->email,
+                    'password' => $dtoUser->password
                 ]);
                 $user->save();
             } else {
@@ -53,6 +39,17 @@ class UserController extends Controller
             }
         } else {
             throw new Exception("No request body was provided.");
+        }
+    }
+
+    private function checkRegData($data)
+    {
+        if (empty($data->email)) {
+            throw new Exception("Email is required.");
+        }
+
+        if (empty($data->password)) {
+            throw new Exception("Password is required.");
         }
     }
 
