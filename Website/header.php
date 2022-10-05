173ff4ba4b81ffba78b87de2148645f213860b20
Milan <milanensink01@gmail.com>
2022-10-05 09:53:49 +0200

    Contact page + header logo fix

--- Website/header.php
+++ Website/header.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <link rel="stylesheet" href="header.css">
-    <a href="images"></a>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Holly Guacamoley</title>
 </head>
@@ -12,7 +11,7 @@
 <header>
     <nav>
         <ul class="nav_links">
-        <img class="logo" src="\gilde-restaurant\Homepage\images\logopng.png">
+        <img class="logo" alt="logo" src="images\logopng.png">
            <li><a href="homepage.php">WELKOM</a></li>
            <li><a href="menu.php">MENU</a></li>
            <li><a href="reserveren.php">RESERVERING</a></li>
