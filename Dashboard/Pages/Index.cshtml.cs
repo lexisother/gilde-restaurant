45dd954850fdcbbcf4c7067d21b8708294f7e59f
Alyxia Sother <alyxia@riseup.net>
2022-10-06 13:29:48 +0200

    [Dashboard] Retrieve API data

--- Dashboard/Pages/Index.cshtml.cs
+++ Dashboard/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Dashboard.DTO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dashboard.Pages;
@@ -6,6 +8,7 @@ namespace Dashboard.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private HttpClient _client = new();
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -14,5 +17,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        var products = _client.GetFromJsonAsync<List<Product>>("https://gdos-api.alyxia.dev/products");
+        ViewData["products"] = products.Result;
     }
 }
