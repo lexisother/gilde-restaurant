e5a62bf8051940ce9a84195b420d8e6ae330f2a6
Alyxia <alyxia@riseup.net>
2022-10-20 13:07:07 +0200

    [Dashboard] Add Cypress tests

---
+++ Dashboard/cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projectId: '6c7c91',
+  fixturesFolder: false,
+  e2e: {
+    baseUrl: 'https://localhost:7184',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
