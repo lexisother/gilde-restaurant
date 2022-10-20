a9315aac1a87ccad0e174291f782b9aa39f997f9
Alyxia <alyxia@riseup.net>
2022-10-20 21:48:05 +0200

    [Dashboard] Log the API endpoint
    [Dashboard] Log the API endpoint
    
    [Dashboard] Log the API endpoint

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -1,3 +1,5 @@
+using Dashboard;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.Logger.LogInformation($"API root path: {Constants.API_BASE}");
 
 app.Run();
