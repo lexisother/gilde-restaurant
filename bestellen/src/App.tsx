92326113ae96b396a4934b4f928adb7fbc07cc0d
Alyxia <alyxia@riseup.net>
2022-12-24 15:47:59 +0100

    [components/PurchaseList] Add basic container

--- bestellen/src/App.tsx
+++ bestellen/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.scss';
 import ProductList from './components/ProductList';
+import PurchaseList from './components/PurchaseList';
+import './App.scss';
 
 export default function App(): JSX.Element {
   return (
     <div className="app-container">
       <ProductList />
+      <PurchaseList />
     </div>
   );
   // return (
---
+++ bestellen/src/components/PurchaseList.tsx
@@ -0,0 +1,5 @@
+import styles from '../styles/PurchaseList.module.scss';
+
+export default function PurchaseList(): JSX.Element {
+  return <div className={styles.purchaseContainer}>hi</div>;
+}
